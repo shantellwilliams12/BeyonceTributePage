386c2331dcb976769995b42cd2cacb5e7c906a1b
shantellwilliams12 <shantell.williams0312@gmail.com>
2021-03-31 19:12:39 -0400

    final touches

--- form.js
+++ form.js
@@ -4,7 +4,7 @@ function validation (){
     var phone = document.getElementById("phone").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
-    var error_message = document.getElementById("error-message").value;
+    var error_message = document.getElementById("error_message");
     var text;
 
     error_message.style.padding = "10px";
